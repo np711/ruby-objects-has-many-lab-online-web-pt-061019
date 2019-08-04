8a744782265799691803e4e3867fcbfd3ec4feeb
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-08-04 18:50:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,15 @@
 class Song 
-  attr_accessor :artist, :name 
+  attr_accessor :name, :artist 
   
   def initialize(name)
     @name = name 
   end 
   
   def artist_name 
-    if artist 
-      self.artist.name 
+    if @artist == nil 
+      return nil  
     else 
-      nil 
+      @artist.name 
     end 
   end 
 end 

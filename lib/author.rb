e7376b2292f108cb5976d3a91d345475ba4d69b1
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-08-04 18:47:19 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -7,22 +7,18 @@ class Author
     @posts = []
   end 
   
-  def posts 
-    @posts 
-    
-  end 
   
   def add_post(post)
     @posts << post 
     post.author = self 
-    @@post_count +=1 
+    @@post_count += 1 
   end 
   
   def add_post_by_title(post_title)
     post = Post.new(post_title)
     @posts << post 
     post.author = self 
-    @@post_count +=1 
+    @@post_count += 1 
   end 
   
   def self.post_count

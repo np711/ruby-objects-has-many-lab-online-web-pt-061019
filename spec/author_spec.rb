7847fc047db89930739cef590ad88fa2d03c5232
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-08-04 18:55:24 +0000

    Automatically backed up by Learn

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -8,7 +8,6 @@ describe "Author" do
       it "is initialized with a name" do
         expect{Author.new("Betty")}.to_not raise_error
       end
-    end
 
     describe "#name" do
       it "has an attr_accessor for name" do

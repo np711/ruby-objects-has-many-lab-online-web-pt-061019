c5d45c796268a9e574c6e640a7c1c3044af1601b
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-08-04 18:44:55 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,23 +1,20 @@
 class Artist 
   attr_accessor :name 
   @@song_count = 0 
+  
   def initialize(name)
     @name = name 
     @songs = []
   end 
   
-  def songs 
-    @songs
-  end 
-  
   def add_song(song)
     @songs << song 
     song.artist = self 
-    @@song_count +=1 
+    @@song_count += 1 
   end 
   
-  def add_song_by_name(song_name)
-    song = Song.new(song_name)
+  def add_song_by_name(name)
+    song = Song.new(name)
     @songs << song 
     song.artist = self 
     @@song_count += 1 

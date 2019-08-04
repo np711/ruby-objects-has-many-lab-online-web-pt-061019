81e78b102e0d0fea105b6feba51b3f0c6cb3e766
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-08-04 18:27:11 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,3 +6,24 @@ class Artist
     @songs = []
   end 
   
+  def songs 
+    @songs
+  end 
+  
+  def add_song(song)
+    @songs << song 
+    song.artist = self 
+    @@song_count +=1 
+  end 
+  
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    @songs << song 
+    song.artist = self 
+    @@song_count += 1 
+  end 
+  
+  def self.song_count
+    @@song_count
+  end 
+  
